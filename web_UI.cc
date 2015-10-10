7f5ecf7142bec5752b2f7c716d8ce80449fc212f
Charles Ofria <charles.ofria@gmail.com>
2015-10-10 08:40:19 -0400

    Added Button functions and file-loaded stub

--- web_UI.cc
+++ web_UI.cc
@@ -7,10 +7,17 @@ namespace UI = emp::web;
 
 UI::Document doc("emp_base");
 
-void DoRestart() { ; }
-void DoStep() { ; }
-void DoPlay() { ; }
-void DoToEnd() { ; }
+VM_UI_base * VMUI;
+extern cHardware * main_hardware;
+
+void DoLoadCode(const std::string & in_code) { ; }
+
+void DoRestart() { VMUI->DoRestart(); }
+void DoStep() { VMUI->DoStep(); }
+void DoPlay() { VMUI->DoPlay(); }
+void DoEnd() { VMUI->DoEnd(); }
+
+bool ParseString(const std::string &);
 
 int main()
 {
@@ -19,14 +26,15 @@ int main()
 
   //    <input type="file" id="infile" name="infile[]" />
   //    <output id="list"></output>
-
+  doc.AddFileInput(DoLoadCode, "load_code");
+  
   doc.AddButton(DoRestart, "Restart", "but_restart")
     .Title("Restart program from beginning").SetWidth(80).Disabled(true);
   doc.AddButton(DoStep, "Step", "but_step")
     .Title("Execute a single instruction in the program").SetWidth(80).Disabled(true);
   doc.AddButton(DoPlay, "Play", "but_play")
     .Title("Continuously execute instructions").SetWidth(80).Disabled(true);
-  doc.AddButton(DoToEnd, "To End", "but_end")
+  doc.AddButton(DoEnd, "To End", "but_end")
     .Title("Execute entire program and display final state").SetWidth(80).Disabled(true);
 
   auto code_div = doc.AddSlate("code_div");
@@ -46,26 +54,10 @@ int main()
   code_table.AddHeader(0,2, "Arg 1");
   code_table.AddHeader(0,3, "Arg 2");
   code_table.AddHeader(0,4, "Arg 3");
-}
 
-
-/*
-VM_UI_base * UI;
-extern cHardware * main_hardware;
-
-bool ParseString(const std::string &);
-
-extern "C" int emkMain()
-{
-  UI = new TubeIC_UI();  // Only build once at the very beginning.
-
-  emk::MethodCallback<VM_UI_base> * restart_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoRestart);
-  emk::MethodCallback<VM_UI_base> * step_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoStep);
-  emk::MethodCallback<VM_UI_base> * play_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoPlay);
-  emk::MethodCallback<VM_UI_base> * end_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoEnd);
-  emk::MethodCallback<VM_UI_base> * play_step_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoPlayStep);
-  
-  Tube_SetButtonCallbacks((int) restart_cb, (int) step_cb, (int) play_cb, (int) end_cb, (int) play_step_cb);
+  // @CAO Load TubeIC for now, but base on a compilation variable.
+  VMUI = new TubeIC_UI();  // Only build once at the very beginning.
+  // VMUI = new tubecode_UI();  // Only build once at the very beginning.
 
   main_hardware = NULL;
 
@@ -73,36 +65,17 @@ extern "C" int emkMain()
 }
 
 
-extern "C" int emkMain_tubecode()
-{
-  UI = new tubecode_UI();  // Only build once at the very beginning.
-
-  emk::MethodCallback<VM_UI_base> * restart_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoRestart);
-  emk::MethodCallback<VM_UI_base> * step_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoStep);
-  emk::MethodCallback<VM_UI_base> * play_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoPlay);
-  emk::MethodCallback<VM_UI_base> * end_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoEnd);
-  emk::MethodCallback<VM_UI_base> * play_step_cb = new emk::MethodCallback<VM_UI_base>(UI, &VM_UI_base::DoPlayStep);
-  
-  Tube_SetButtonCallbacks((int) restart_cb, (int) step_cb, (int) play_cb, (int) end_cb, (int) play_step_cb);
-
-  main_hardware = NULL;
-
-  return 0;
-}
-
-
-extern "C" int emkLoadString(char * _string)
+extern "C" int LoadCode(char * _string)
 {
   // Initialize hardware object and UI.
-  if (main_hardware != NULL) delete main_hardware;  // If we already have hardware, clean it up an build a new one.
-  main_hardware = new cHardware();
+  if (main_hardware != NULL) delete main_hardware;  // Remove existing hardware, if any
+  main_hardware = new cHardware();                  // Build new hardware.
 
   // Parse the input code (which will automatically load it into the main hardware.
   ParseString(_string);
 
   // Setup the UI with the newly loaded hardware.
-  UI->SetupHardware(main_hardware);
+  VMUI->SetupHardware(main_hardware);
 
   return 0;
 }
-*/
