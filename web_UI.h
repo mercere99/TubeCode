f3e957d69c1624ab13e601d1b550e702e1ac9cc7
Charles Ofria <charles.ofria@gmail.com>
2015-10-16 12:12:27 -0400

    First pass on converting web interface to Empirical

--- web_UI.h
+++ web_UI.h
@@ -19,13 +19,23 @@ void DoLoadCode(const std::string & in_code) {
   LoadCode(in_code);
 }
 
+// Some general constants.
+static const std::string inst_bg = "#f0f0f0";  // What color should general cells be?
+static const std::string IP_bg = "#d0f0d0";    // What color should active cells be?
+static const std::string title_bg = "#CCCCFF"; // What color should the var title background be?
+
+static const int var_table_width = 500;
+static const int var_table_col_count = 4;
+
 class VM_UI_base {
 protected:
   cHardware * hardware;
   UI::Document doc;
+
+  bool is_paused;
   
 public:
-  VM_UI_base() : hardware(NULL), doc("emp_base")
+  VM_UI_base() : hardware(NULL), doc("emp_base"), is_paused(true)
   {
     doc << "<h1>Welcome to the TubeIC virtual machine</h1>"
         << "<p>Choose a Tube Intermediate Code file that you would like to load and run.</p>";
@@ -51,7 +61,7 @@ public:
     UI::Table code_table(1,5,"code");
     code_div << code_table;
     code_table.SetCSS("border-collapse", "collapse");
-    code_table.SetBackground("#F0F0F0");
+    code_table.SetBackground(inst_bg);
     
     code_table.AddHeader(0,0, "Line");
     code_table.AddHeader(0,1, "Instruction");
@@ -59,85 +69,110 @@ public:
     code_table.AddHeader(0,3, "Arg 2");
     code_table.AddHeader(0,4, "Arg 3");
 
+    UI::Slate rt_column("rt_column");
+    UI::Slate console("console");
+    UI::Slate var_div("var_div");
+    UI::Table var_table(var_table_col_count,1, "var_table");
+
+    console.SetSize(500, 200).SetFloat("left").SetPadding(5)
+      .SetColor("white").SetBackground("black")
+      .SetOverflow("auto").SetBorder("4px solid white");
+
+    var_div.SetSize(var_table_width,290).SetFloat("left").SetPadding(5)
+      .SetColor("black").SetBackground("white")
+      .SetOverflow("auto");
+    
+    var_table.GetCell(0,0).SetHeader() << "Scalar Variables";
+    var_table.GetCell(2,0).SetHeader() << "Array Variables";
+
+    var_div << var_table;
+    rt_column << console;
+    rt_column << var_div;
+    doc << rt_column;
+    
   }
   virtual ~VM_UI_base() { ; }
 
   void UpdateCode() {
-    std::string inst_bg = "#f0f0f0";  // What color should general cells be?
-    std::string IP_bg = "#d0f0d0";    // What color should IP cells be?
-
-    std::stringstream ss;
-    ss << "<table style=\"background-color:" << inst_bg << ";\">"
-       << "<tr><th>Line</th><th>Instruction</th><th>Arg 1</th><th>Arg 2</th><th>Arg 3</th></tr>";
-
-    const std::map<std::string, int> & label_map = hardware->GetLabelMap();
+    UI::Table code_table = doc.Table("code");
 
     // Reorganize label_map to be sorted by position, NOT name
-    std::multimap<int, std::string> position_map = emp::flip_map(label_map);
+    std::multimap<int, std::string> position_map = emp::flip_map(hardware->GetLabelMap());
 
     // Be ready to step through position map to identify when we've hit a label!
     auto label_it = position_map.begin();
 
+    // Resize the table appropriately.
+    code_table.Rows( hardware->GetNumInsts() + position_map.size() + 1 );
+
     // Step through all of the instructions in the program
+    int cur_row = 1;
     for (int inst_id = 0; inst_id < hardware->GetNumInsts(); inst_id++) {
+      // Clear the current row.
+      code_table.GetRow(cur_row).Clear();
+      code_table.GetRow(cur_row).SetBackground("white");
+
       // Check if there is a label we need to print.
       while (label_it != position_map.end() && inst_id == label_it->first) {
-        ss << "<tr style=background-color:white><td colspan=5>&nbsp;&nbsp;" << label_it->second << ":</tr>";
+        code_table.GetCell(cur_row, 0).SetColSpan(5) << label_it->second << ":";
         label_it++;
+        cur_row++;
       }
 
       // Instruction
       cInst_Base * inst = hardware->GetInst(inst_id);
 
       // Make the current instruction (at the IP) a different color.
-      if (inst_id == hardware->GetIP()) ss << "<tr style=background-color:" << IP_bg << ">";
-      else ss << "<tr>";      
-
-      // Print out the information about the current instruction.
-      ss << "<td>" << inst_id
-         << "<td>" << inst->GetName();
-
-      // Arguments
-      for (int i = 0; i < 3; i++) {
-        ss << "<td>" << inst->GetArgString(i);
+      if (inst_id == hardware->GetIP()) {
+        code_table.GetRow(cur_row).SetBackground(IP_bg);
       }
-      ss << "</tr>";
+        
+      // Update the information about the current instruction.
+      code_table.GetCell(cur_row, 0) << inst_id;
+      code_table.GetCell(cur_row, 1) << inst->GetName();
+
+      // If there are arguments, update them as well.
+      for (int i = 0; i < 3; i++) {
+        code_table.GetCell(cur_row, i+2) << inst->GetArgString(i);
+      }
+
+      cur_row++;
     }
 
     // See if there are any final labels to include
     while (label_it != position_map.end()) {
-      ss << "<tr style=background-color:white><td colspan=5>&nbsp;&nbsp;"
-         << label_it->second << ":</tr>";
+      code_table.GetCell(cur_row, 0).SetColSpan(5) << label_it->second << ":";
       label_it++;
+      cur_row++;
     }
-
-    ss << "</table>";
 
     // Where should we scroll to on the screen?
     // Aim for top of scroll to be a few lines above IP.
-    float scroll_frac = ((float) (hardware->GetIP() - 3)) / (float) hardware->GetNumInsts();
-    if (scroll_frac < 0.0) scroll_frac = 0.0;
 
-    EM_ASM_ARGS({
-        var code = Pointer_stringify($0);
-        var code_obj = document.getElementById("code");
-        code_obj.innerHTML = code;
-        code_obj.scrollTop = $1 * code_obj.scrollHeight;
-        var cycle_obj = document.getElementById("cycle_count");
-        cycle_obj.innerHTML = "&nbsp;&nbsp;&nbsp;Cycles Used = " + $2;
-    }, ss.str().c_str(), scroll_frac, hardware->GetExeCount());
+    // float scroll_frac = ((float) (hardware->GetIP() - 3)) / (float) hardware->GetNumInsts();
+    // if (scroll_frac < 0.0) scroll_frac = 0.0;
+
+    // EM_ASM_ARGS({
+    //     var code = Pointer_stringify($0);
+    //     var code_obj = document.getElementById("code");
+    //     code_obj.innerHTML = code;
+    //     code_obj.scrollTop = $1 * code_obj.scrollHeight;
+    //     var cycle_obj = document.getElementById("cycle_count");
+    //     cycle_obj.innerHTML = "&nbsp;&nbsp;&nbsp;Cycles Used = " + $2;
+    // }, ss.str().c_str(), scroll_frac, hardware->GetExeCount());
 
 
   }
 
   
   void UpdateConsole() {
-    EM_ASM_ARGS({
-        var msg = Pointer_stringify($0);
-        var console_obj = document.getElementById("console");
-        console_obj.innerHTML = "<pre>" + msg + "</pre>";
-        console_obj.scrollTop = console_obj.scrollHeight;
-    }, hardware->GetMessages().c_str());
+    UI::Slate console = doc.Table("console");
+
+    console.ClearChildren();
+    console << hardware->GetMessages();
+
+    // @CAO Make sure to scroll to console bottom!
+    // console_obj.scrollTop = console_obj.scrollHeight;
   }
 
   // The main different between hardware types is available variables
@@ -159,11 +194,8 @@ public:
     hardware->Restart();
     UpdateUI();
 
-    // Make sure we pause the run since we jumpped back to the beginning.
-    EM_ASM({
-        tube_cb.is_paused = true;
-        document.getElementById("play_button").innerHTML = "Play";
-    });
+    is_paused = true;
+    doc.Button("but_play").Label("Play");
   }
 
   void DoStep() {
@@ -172,41 +204,34 @@ public:
   }
 
   void DoPlay() {
-    EM_ASM({
-        tube_cb.is_paused = !tube_cb.is_paused;
-        if (tube_cb.is_paused) document.getElementById("play_button").innerHTML = "Play";
-        else {
-          emkJSDoCallback(tube_cb.play_step, 0);
-          document.getElementById("play_button").innerHTML = "Pause";
-        }
-    });
+    is_paused = !is_paused;
+    if (is_paused) doc.Button("but_play").Label("Play");
+    else {
+      doc.Button("but_play").Label("Pause");
+      // @CAO Start the playing!
+    }
   }
 
   void DoEnd() {
     hardware->Run();    
     UpdateUI();
 
-    // Make sure we pause the run since end has been reached!
-    EM_ASM({
-        tube_cb.is_paused = true;
-        document.getElementById("play_button").innerHTML = "Play";
-    });
+    is_paused = true;
+    doc.Button("but_play").Label("Play");
   }
 
   void DoPlayStep() {
     // If we've run off the end, automatically pause.
     if (hardware->GetIP() >= hardware->GetNumInsts()) {
-      EM_ASM({
-          tube_cb.is_paused = true;
-          document.getElementById("play_button").innerHTML = "Play";
-      });
+      is_paused = true;
+      doc.Button("but_play").Label("Play");
     }
-    EM_ASM({
-        if (tube_cb.is_paused == false) {
-          emkJSDoCallback(tube_cb.step, 0);
-          window.setTimeout(function() { emkJSDoCallback(tube_cb.play_step, 0); }, 250);
-        }
-    });
+    if (is_paused == false) {
+      // @CAO Play a single step!
+      // emkJSDoCallback(tube_cb.step, 0);
+      // emp::DelayCall()
+      // window.setTimeout(function() { emkJSDoCallback(tube_cb.play_step, 0); }, 250);
+    }
   }
 
 };
@@ -218,58 +243,78 @@ public:
   ~TubeIC_UI() { ; }
 
   void UpdateVars() {
-    // Basic layout settings
-    const std::string title_bg = "#CCCCFF";
-    const int table_width = 500;
-    const int col_count = 4;
-    const int col_width = table_width / col_count;
-
-    // Setup the code for the table
-    std::stringstream ss;
-    ss << "<table width=" << table_width << "px>"
-       << "<tr style=\"background-color:#CCCCFF\"><th colspan=" << col_count
-       << ">Scalar Variables</th></tr>";
-
     const std::map<int, cVar> & var_map = hardware->GetVarMap();
-    
-    // Print the scalar variables into the table.
-    int var_count = 0;
-    for (auto var_it = var_map.begin(); var_it != var_map.end(); var_it++) {
-      if (var_count % col_count == 0) ss << "<tr>";
-      ss << "<td width=" << col_width << "px>s" << var_it->first << " = " << var_it->second.AsInt();
-      var_count++;
-    }
-
-    // Leave blanks to fill out the row.
-    while (var_count % col_count != 0) {
-      ss << "<td width=" << col_width << "px>&nbsp;";
-      var_count++;
-    }
-    
-    // Print the array variables into the table.
-    ss << "<tr style=\"background-color:#CCCCFF\"><th colspan=" << col_count
-       << ">Array Variables</th></tr>";
-
     const std::map<int, cArray> & array_map = hardware->GetArrayMap();
     
-    // Print the array variables into the table.
-    for (auto var_it = array_map.begin(); var_it != array_map.end(); var_it++) {
-      ss << "<tr><td colspan=" << col_count << ">a" << var_it->first << " = [ ";
-      for (int i = 0; i < var_it->second.GetSize(); i++) {
-        if (i > 0) ss << ", ";
-        ss << var_it->second.GetIndex(i);
-      }
-      //<< var_it->second.AsInt();
-      ss << " ]</td></tr>";
+    // Basic layout settings
+    const int col_width = var_table_width / var_table_col_count;
+    const int num_vars = (int) var_map.size();
+    const int num_arrays = (int) array_map.size();
+    
+    const int var_rows = (num_vars+3)/ var_table_col_count;
+    const int array_rows = (num_arrays+3)/ var_table_col_count;
+
+    const int total_rows = var_rows + array_rows + 3;
+    
+    UI::Table var_table = doc.Table("var_table");
+    var_table.Rows(total_rows);
+
+    // Setup the header for scalars
+    int cur_row = 0;
+    var_table.GetRow(cur_row).SetBackground(title_bg);
+    var_table.GetCell(cur_row, 0).SetColSpan(var_table_col_count).SetHeader() << "Scalar Variables";
+    cur_row++;
+
+    // Print the scalar variables into the table.
+    int cur_col = 0;
+    for (auto var_it = var_map.begin(); var_it != var_map.end(); var_it++) {
+      if (var_table_col_count == 4) { cur_row++; cur_col = 0; }
+      var_table.GetCell(cur_row, cur_col).SetWidth(col_width)
+        << "s" << var_it->first << " = " << var_it->second.AsInt();
+      cur_col++;
     }
+    cur_row++;
+
+    // Skip a line.
+    var_table.GetCell(cur_row, 0).SetColSpan(var_table_col_count) << "&nbsp;";
+    cur_row++;
 
 
-    ss << "</table>";
+    // Setup header for array variables
+    var_table.GetRow(cur_row).SetBackground(title_bg);
+    var_table.GetCell(cur_row, 0).SetColSpan(var_table_col_count).SetHeader() << "Array Variables";
+    cur_row++;
 
-    EM_ASM_ARGS({
-        var var_info = Pointer_stringify($0);
-        document.getElementById("vars").innerHTML = var_info;
-    }, ss.str().c_str());
+    // Print the array variables into the table.
+    cur_col = 0;
+    for (auto var_it = array_map.begin(); var_it != array_map.end(); var_it++) {
+      if (var_table_col_count == 4) { cur_row++; cur_col = 0; }
+      var_table.GetCell(cur_row, cur_col).SetWidth(col_width)
+        << "s" << var_it->first << " = [ ";
+      for (int i = 0; i < var_it->second.GetSize(); i++) {
+        if (i>0) var_table << ", ";
+        var_table << var_it->second.GetIndex(i);
+      }
+
+      var_table << "]";
+      cur_col++;
+    }
+    cur_row++;
+
+
+    
+
+    // Print the array variables into the table.
+    // for (auto var_it = array_map.begin(); var_it != array_map.end(); var_it++) {
+    //   ss << "<tr><td colspan=" << col_count << ">a" << var_it->first << " = [ ";
+    //   for (int i = 0; i < var_it->second.GetSize(); i++) {
+    //     if (i > 0) ss << ", ";
+    //     ss << var_it->second.GetIndex(i);
+    //   }
+    //   //<< var_it->second.AsInt();
+    //   ss << " ]</td></tr>";
+    // }
+
   }
 
   
